fec7434ea71a74b56a5555f3e70e1396ca61fd92
Yihang Wang <31209506+wyh2001@users.noreply.github.com>
2025-07-04 02:17:52 -0400

    Add blazor as frontend

---
+++ client/Toko.Web/Program.cs
@@ -0,0 +1,33 @@
+using Toko.Web.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+builder.Services.AddHttpClient("ServerAPI", client =>
+{
+    //client.BaseAddress = new Uri(builder.Configuration["ServerAPI:BaseUrl"]);
+    client.BaseAddress = new Uri("https://localhost:7057");
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+
+app.UseAntiforgery();
+
+app.MapStaticAssets();
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
