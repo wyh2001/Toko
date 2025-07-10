bfff9b2a565daf0594880549787b7c47b6b238d1
Yihang Wang <31209506+wyh2001@users.noreply.github.com>
2025-07-09 22:40:42 -0400

    Modify comments in TrackSegment class for clarity

--- src/Toko/Models/RaceMap.cs
+++ src/Toko/Models/RaceMap.cs
@@ -12,9 +12,8 @@ namespace Toko.Models
         public class TrackSegment
         {
             public TileType Type { get; set; }       
-            public required List<List<Point>> LaneCells { get; set; } // LaneCells[i] 就是第 i 车道上的所有格子
-            public int LaneCount { get; set; }       // 直道=4，弯道=2
-                                                     //public List<Point> Cells { get; set; }   // 每条车道的 (x,y) 网格坐标
+            public required List<List<Point>> LaneCells { get; set; } // LaneCells[i] is all cells in lane i of this segment
+            public int LaneCount { get; set; }
         }
     }
 
